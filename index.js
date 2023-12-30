9324b288af5035b8691e508388acc401aeab1140
rohitgaidhane <rohitgaidhane123@gmail.com>
2023-12-30 22:12:49 +0530

    Array basic sum of age

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let arr = [
+    { name: "user1", age: 28, isVerified: true }, // arr[0]
+    { name: "user2", age: 25, isVerified: false }, // arr[1]
+    { name: "user3", age: 38, isVerified: true }, // arr[2]
+    { name: "user4", age: 25, isVerified: true }, // arr[3]
+  ];
+  
+  function sum() {
+    let sum_Age = 0; 
+    for (let i = 0; i < arr.length; i++) {
+      sum_Age =    sum_Age + arr[i].age;
+    }
+    return sum_Age
+  }
+  
+  let age = sum();
+  console.log("Sum of all user Age is "+age);
